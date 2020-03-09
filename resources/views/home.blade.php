4d59ea19b2d4e8b1f43cdce2ad2f5f4fa0871d4b
alicia131 <aliciarodriguez131@gmail.com>
2020-03-08 19:00:56 -0500

    ttt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,21 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>UBold - Responsive Admin Dashboard Template</title>
+        <title>Pagina principal</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        @include('layouts.estilos')
+           <!-- App favicon -->
+   <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
+
+   <!-- Plugins css -->
+   <link href="{{asset('assets/libs/flatpickr/flatpickr.min.css')}}" rel="stylesheet" type="text/css"  />
+   <!-- App css -->
+   <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
@@ -652,7 +660,21 @@
 
 
     <!-- Vendor js -->
-    @include('layouts.scripts')
+    <script src="{{asset('assets/js/vendor.min.js')}}"></script>
+
+    <!-- Plugins js-->
+    <script src="{{asset('assets/libs/flatpickr/flatpickr.min.js')}}"></script>
+    <script src="{{asset('assets/libs/jquery-knob/jquery.knob.min.js')}}"></script>
+    <script src="{{asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{asset('assets/libs/flot-charts/jquery.flot.js')}}"></script>
+    <script src="{{asset('assets/libs/flot-charts/jquery.flot.time.js')}}"></script>
+    <script src="{{asset('assets/libs/flot-charts/jquery.flot.tooltip.min.js')}}"></script>
+    <script src="{{asset('assets/libs/flot-charts/jquery.flot.selection.js')}}"></script>
+    <script src="{{asset('assets/libs/flot-charts/jquery.flot.crosshair.js')}}"></script>
+      <!-- Dashboar 1 init js-->
+   <script src="{{asset('assets/js/pages/dashboard-1.init.js')}}"></script>
+     <!-- App js-->
+     <script src="{{asset('assets/js/app.min.js')}}"></script>
 
     </body>
 
